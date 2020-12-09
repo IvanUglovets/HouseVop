f532b1f3eb1022f9a66cc0def960a81c49b39b68
IvanUglovets <75724222+IvanUglovets@users.noreply.github.com>
2020-12-09 11:57:24 +0300

    new HouseVop

---
+++ js/map.js
@@ -0,0 +1,7 @@
+function initMap(){
+            var opt ={
+                center: {lat:52.3560247, lng:30.4051694},
+                zoom: 12
+            }
+     var map = new google.maps.Map(document.getElementById("map"),opt);
+ }
